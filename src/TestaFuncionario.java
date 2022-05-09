b6c2541f8afd9f108e5a9402e4ed3685ac0d86f9
Miguel-Matheus <Miguel-Matheus@test.com>
2022-05-09 10:52:16 -0300

    exercicio 10

---
+++ src/TestaFuncionario.java
@@ -0,0 +1,11 @@
+
+public class TestaFuncionario {
+	
+	public static void main(String[] args) {
+		Funcionario referencia = new Funcionario();
+		
+		referencia.cargo = "Professor";
+		referencia.sal_func = 3000;
+	}
+
+}
